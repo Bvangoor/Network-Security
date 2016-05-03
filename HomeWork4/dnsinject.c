5384a9b9a212bd48c18ca34ea41785329af2f6c6
Bharath <vangoor.bharath@gmail.com>
2016-05-03 17:06:44 -0400

    1

--- HomeWork4/dnsinject.c
+++ HomeWork4/dnsinject.c
@@ -67,24 +67,13 @@ struct sniff_udp {
 #define SIZE_UDP        8               /* length of UDP header */
 
 struct DNS_HEADER {
-    unsigned short id; // identification number
-    unsigned char rd :1; // recursion desired
-    unsigned char tc :1; // truncated message
-    unsigned char aa :1; // authoritive answer
-    unsigned char opcode :4; // purpose of message
-    unsigned char qr :1; // query/response flag
-      
-    unsigned char rcode :4; // response code
-    unsigned char cd :1; // checking disabled
-    unsigned char ad :1; // authenticated data
-    unsigned char z :1; // its z! reserved
-    unsigned char ra :1; // recursion available
-      
-    unsigned short q_count; // number of question entries
-    unsigned short ans_count; // number of answer entries
-    unsigned short auth_count; // number of authority entries
-    unsigned short add_count; // number of resource entries
-};
+	unsigned short id; // identification number
+	unsigned short flags; 
+	unsigned short q_count; // number of question entries
+	unsigned short ans_count; // number of answer entries
+	unsigned short auth_count; // number of authority entries
+	unsigned short add_count; // number of resource entries
+}__attribute__((packed));
 
 //Constant sized fields of query structure
 struct QUESTION {
@@ -167,8 +156,7 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	int size_ip;
 	bool frthr_analysis = false;
 	unsigned offset = 0;
-	struct DNS_HEADER *DNS_HEADER=&DNS_HEADER;
-	struct dns *dns;
+	struct DNS_HEADER *dns;
 	struct QUESTION *question;
 
 	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
@@ -209,21 +197,7 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 			offset = SIZE_ETHERNET + size_ip + SIZE_UDP;  
 			dns = (struct DNS_HEADER *)(packet + offset);
  
-			    /*Printing QR Code*/
-			printf("DNS QR : %d\n", DNS_HEADER->qr & 1); 
-     
-			switch (DNS_HEADER->qr)
-			{
-    				case 0:
-    					printf("QR Code is Query\t");
-    					break;  
-    				case 1:
-    					printf("QR Code is Answer\t");
-					break;
-				default :
-					printf("Stupid Thing\n");
-					break;
-    			}
+     			printf("DNS ID : %d\n", ntohs(dns->id));
 		}
 	}
 }
