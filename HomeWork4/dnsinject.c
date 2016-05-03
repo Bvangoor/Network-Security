10c9e52c6ed40404577ed8f35bacf674fbd7ee69
bvangoor <bvangoor@cs.stonybrook.edu>
2016-05-03 16:59:38 -0400

    playing with UDP payload

--- HomeWork4/dnsinject.c
+++ HomeWork4/dnsinject.c
@@ -14,6 +14,26 @@
 #include <netinet/in.h>
 #include <net/if.h>
 
+
+/* Offsets of fields in the DNS header. */
+#define DNS_ID      0
+#define DNS_FLAGS   2
+#define DNS_QUEST   4
+#define DNS_ANS     6
+#define DNS_AUTH    8
+#define DNS_ADD     10
+ 
+/* Length of DNS header. */
+#define DNS_HDRLEN  12
+ 
+//Type field of Query and Answer
+#define T_A 1 /* host address */
+#define T_NS    2 /* authoritative server */
+#define T_CNAME 5 /* canonical name */
+#define T_SOA   6 /* start of authority zone */
+#define T_PTR   12 /* domain name pointer */
+#define T_MX    15 /* mail routing information */
+
 /* ethernet headers are always exactly 14 bytes [1] */
 #define SIZE_ETHERNET 14
 
@@ -45,6 +65,56 @@ struct sniff_udp {
 };
 
 #define SIZE_UDP        8               /* length of UDP header */
+
+struct DNS_HEADER {
+    unsigned short id; // identification number
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+      
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+      
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+
+//Constant sized fields of query structure
+struct QUESTION {
+        unsigned short qtype;
+        unsigned short qclass;
+};
+      
+//Constant sized fields of the resource record structure
+    struct R_DATA
+    {
+        unsigned short type;
+        unsigned short _class;
+        unsigned int ttl;
+        unsigned short data_len;
+    };
+      
+//Pointers to resource record contents
+    struct RES_RECORD
+    {
+        unsigned char *name;
+        struct R_DATA *resource;
+        unsigned char *rdata;
+    };
+      
+//Structure of a Query
+    typedef struct
+    {
+        unsigned char *name;
+        struct QUESTION *ques;
+    } QUERY;
 
 struct map {
         char hostnames[100][100];
@@ -96,14 +166,18 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	const struct sniff_udp *udp;		/* The UDP header*/
 	int size_ip;
 	bool frthr_analysis = false;
+	unsigned offset = 0;
+	struct DNS_HEADER *DNS_HEADER=&DNS_HEADER;
+	struct dns *dns;
+	struct QUESTION *question;
 
 	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
 	size_ip = IP_HL(ip)*4;
 
-	if (size_ip < 20) {
-		printf("   * Invalid IP header length: %u bytes\n", size_ip);
-		return;
-	}
+//	if (size_ip < 20) {
+//		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+//		return;
+//	}
 	
 	switch(ip->ip_p) {
 		case IPPROTO_TCP:
@@ -122,17 +196,36 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 	}
 
 	if (frthr_analysis) {
-		/* print source and destination IP addresses */
-		printf("       From: %s\n", inet_ntoa(ip->ip_src));
-		printf("         To: %s\n", inet_ntoa(ip->ip_dst));
 	
 		/* define/compute udp header offset */
                 udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
 
-                printf("   Src port: %d\n", ntohs(udp->uh_sport));
-                printf("   Dst port: %d\n", ntohs(udp->uh_dport));		
+		if (ntohs(udp->uh_dport) == 53) {
+			/* print source and destination IP addresses */
+			printf("       From: %s\n", inet_ntoa(ip->ip_src));
+			printf("         To: %s\n", inet_ntoa(ip->ip_dst));
+			printf("   Src port: %d\n", ntohs(udp->uh_sport));
+			printf("   Dst port: %d\n", ntohs(udp->uh_dport));
+			offset = SIZE_ETHERNET + size_ip + SIZE_UDP;  
+			dns = (struct DNS_HEADER *)(packet + offset);
+ 
+			    /*Printing QR Code*/
+			printf("DNS QR : %d\n", DNS_HEADER->qr & 1); 
+     
+			switch (DNS_HEADER->qr)
+			{
+    				case 0:
+    					printf("QR Code is Query\t");
+    					break;  
+    				case 1:
+    					printf("QR Code is Answer\t");
+					break;
+				default :
+					printf("Stupid Thing\n");
+					break;
+    			}
+		}
 	}
-	
 }
 
 
